5d3a07d53d4ae396a89aa07500c8034f3e9e5224
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2017-05-12 11:06:00 +0700

    add_config_angular_for_posts

---
+++ app/assets/javascripts/angular/controllers/posts_controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var app = angular.module('myPageApp');
+
+app.controller('PostsController', PostsController);
+
+function PostsController(){
+  var post = this;
+};
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,6 @@
+class PostsController < ApplicationController
+  layout "mypage"
+
+  def index
+  end
+end
