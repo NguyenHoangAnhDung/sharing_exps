9844de918eab6e368e674af9ff39347b28d5c958
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2018-07-14 07:24:56 +0700

    Implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,14 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  scope :rails_portfolio_items, -> { where subtitle: "Rails" }
+  scope :angular_portfolio_items, -> { where subtitle: "Angular" }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+
+  end
 end
