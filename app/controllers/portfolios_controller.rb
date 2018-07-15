c0c4acb77dcce06615bcd9d6512d83862c54e47f
NguyenHoangAnhDung <nguyen.hoang.anh.dung@framgia.com>
2018-07-15 08:34:44 +0700

    Added guest user for current_user

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,8 +13,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio)
-      .permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolios_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -33,8 +32,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find params[:id]
     
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body, 
-        technologies_attributes: [:name]))
+      if @portfolio_item.update(prortfolio_params)
         format.html { redirect_to portfolios_path, notice: "The record successfully updated." }
       else
         format.html { render :edit }
@@ -53,5 +51,12 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: "Record was removed." }
     end
+  end
+
+  private
+  
+  def prortfolio_params
+    params.require(:portfolio)
+      .permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end
 end
